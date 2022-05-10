e67247c51c5311b95d0fb0516a173bf96822110a
laurapry22 <82665239+laurapry22@users.noreply.github.com>
2022-05-10 16:02:00 -0500

    fixed issues with styling

---
+++ CS268_Project1/templates/eventstop.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>OmTech</title>
+  <link rel="icon" type="image/x-icon" href="images/myIcon.ico">
+  <link rel="stylesheet" href="css/styles.css" />
+  <link rel="stylesheet" href="css/edit.css" />
+  <link rel="stylesheet" href="css/events.css" />
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+</head>
+<body>
+  <header>
+    <img class="logo" src="images/omtech_old_logo_2.png" alt="OmTech Logo">
+    <h2>Organization for Minorities in TECHnology</h2>
+  </header>
+  <div class="header">
+    <div class="navBar">
+        <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="aboutus.html">About</a></li>
+            <li><a href="events.php">Events</a></li>
+            <li><a href="meettheboard.php">Board</a></li>
+            <li><a href="news.php">News</a></li>
+            <li><a href="contactus.html">Contact Us</a></li>
+            <li><a href="loginpage.php">Admin Login</a></li>
+        </ul>
+    </div>
+    <div class="dropDown">
+        <button class="menuButton">Menu</button>
+        <div class="dropDownChild">
+            <ul>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="aboutus.html">About</a></li>
+                <li><a href="events.php">Events</a></li>
+                <li><a href="meettheboard.php">Board</a></li>
+                <li><a href="news.php">News</a></li>
+                <li><a href="contactus.html">Contact Us</a></li>
+                <li><a href="loginpage.php">Admin Login</a></li>
+            </ul>
+        </div>
+    </div>
+  </div>
