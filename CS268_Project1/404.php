04c539788f03860c5c636ac5939fd73eb10947fe
maltreuter <maltreuter@gmail.com>
2022-05-11 16:54:42 -0500

    Update 404.php

--- CS268_Project1/404.php
+++ CS268_Project1/404.php
@@ -1,11 +1,13 @@
 <?php
-$css = ["styles.css", "events.css"];
+$css = ["styles.css", "aboutus.css"];
 include('templates/top.php');
 ?>
 
-<div class="cards">
-    <div class="card">
-        <h2>404</h2>
+<div class="card" style="justify-content: center">
+    <div class="container">
+        <div class="textSection">
+            <h2 class="aboutHeader">404</h2>
+        </div>    
     </div>
 </div>
 
